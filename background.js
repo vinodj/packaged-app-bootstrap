09fcfed47c5048fe4d87ed10c1073572c31eb8f6
Burke Holland <burkeholland@gmail.com>
2012-06-24 10:09:20 -0500

    Add FileSystem access

--- background.js
+++ background.js
@@ -1,17 +1,14 @@
 chrome.experimental.app.onLaunched.addListener(function() { 
   
-  function onWindowLoaded(win) {
-  	console.log(win)
-  }
+  // create a new window and position it with a fixed size
+  var win = chrome.appWindow.create('main.html', { 
+    width: 1024, 
+    height: 870,
+    minWidth:900,
+    minHeight:800,
+    left:500,
+    top:500
+  });
 
-  var win = chrome.appWindow.create('index.html', { 
-  	width: 1024, 
-  	height: 870,
-  	minWidth:900,
-  	minHeight:800,
-  	left:500,
-  	top:500
-  	}, 
-  	onWindowLoaded);
 });
 
---
+++ js/main.js
@@ -0,0 +1,55 @@
+MYAPP = MYAPP || {};
+
+MYAPP.main = (function($) {
+	
+	// get a reference to the iframe that holds the app
+	iframe = document.getElementById("iframe");
+
+	// initialize the postman. he's off of MYAPP. He needs to know
+    // who the recipient is. It's the iframe where the app lives.
+    MYAPP.postman.init(iframe.contentWindow);
+
+    // subscribe to the /select/image message and return an image using
+    // the native api file picker in chrome packaged apps
+    $.subscribe("/notification/show", function(message) {
+    
+        var notification = webkitNotifications.createNotification('icon_16.png', message.title, message.body);
+        notification.show();
+    });
+
+    // subscribe to the /select/image message and return an image using
+    // the native api file picker in chrome packaged apps
+    $.subscribe("/select/file", function() {
+    
+        chrome.fileSystem.chooseFile({ type: "chooseFile"}, function(entry) {
+    
+          // this gives us a file entry. We just need to read it.
+          entry.file(function(file) {
+    
+            // create a new file reader
+            var reader = new FileReader();
+    
+            // create an event for when the file is done reading
+            reader.onloadend = function(e) {
+              // tell the postman to deliver this to the sandbox
+              $.publish("/postman/deliver", [ { message: { src: this.result } }, "/file/loaded"])
+            }
+    
+            // read the file as a data URL
+            reader.readAsDataURL(file);
+        
+          });
+        
+        });   
+    });
+
+    // subscribe to the /app/ready event which tells us that the app is ready
+    $.subscribe("/app/ready", function() {
+
+      // create a new message to send to whatever is listenting
+      // to /greeting/hello inside the sandbox
+      $.publish("/postman/deliver", [{ message: { text: "Hello From The Extension" }}, "/greeting/hello"]);
+
+    });
+
+})(jQuery);
