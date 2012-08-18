0014d3f9a0816be2bba5004bdad4869d3ca8b58c
Burke Holland <burkeholland@gmail.com>
2012-08-18 13:33:04 -0500

    API changes for opening packaged app window.

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
-chrome.experimental.app.onLaunched.addListener(function() { 
+chrome.app.runtime.onLaunched.addListener(function() { 
   
   // create a new window and position it with a fixed size
-  var win = chrome.appWindow.create('main.html', { 
+  var win = chrome.app.window.create('main.html', { 
     width: 1024, 
     height: 870,
     minWidth:900,
